2a5c25436cab46b6261b5b4f6211e27e6aa72bc8
Pavat Pichetsin <pavatpichetsin@outlook.com>
2024-03-23 14:59:59 +0000

    Implemented the youtube search tool. I added the 'open' crate to add tools to open url. The code I wrote concatenates the argument into the youtube search url and opens it on the browser

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use clap::Parser;
+use open;
 #[derive(Parser)]
 struct Cli {
     pattern: String,
 }
 
 fn main() {
+    let url = "https://www.youtube.com/results?search_query=";
     let args: Cli = Cli::parse();
+    let search = url.to_owned() + &args.pattern;
+    match open::that(search) {
+        Ok(()) => println!("success!"),
+        Err(err) => eprintln!("error: {}", err),
+    }
 }
